5a533ff2d21fd0c67e38eae9589ff83473967586
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2026-01-14 16:45:31 +0100

    feat(web): Make station info drawer non-modal
    The station info drawer will now appear as a non-modal component,
    allowing interaction with the map while it's open. This improves
    usability by not blocking the entire screen.

--- apps/web/components/station-info.tsx
+++ apps/web/components/station-info.tsx
@@ -324,6 +324,8 @@ export default function StationInfo() {
       setActiveSnapPoint={setSnap}
       open={isOpen}
       onClose={clearStation}
+      modal={false}
+      key="non-modal"
       noBodyStyles
     >
       <DrawerContent
